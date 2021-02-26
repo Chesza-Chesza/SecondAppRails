5ab58379deb0e583e48aeb322c57c60c266df9b0
Shirley Alvarado <shirley@codecrc.com>
2021-02-25 20:11:57 -0600

    Pages and routes added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get  "users/login"
-  get  "pages/about_us"
-  root "pages#index"
+  get 'users', to: 'users#login'
+  get 'pages', to: 'pages#index'
+  get 'pages', to: 'pages#about_us'
+  root 'pages#index'
 end
